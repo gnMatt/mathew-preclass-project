68d48f513a49fed15aae1b8b4cfb40e806e35357
gnMatt <gmdods10@gmail.com>
2023-05-15 16:36:01 +0800

    activity2: get full name in a hash

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,15 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+
+names = {first_name: "Mathew", middle_name: "Coronado", last_name: "Dorado"}
+def full_name(names)
+  f_name = ""
+  names.each do |key, name|
+    f_name += name + ' '
+  end
+end
+
+
+  p full_name(names)
